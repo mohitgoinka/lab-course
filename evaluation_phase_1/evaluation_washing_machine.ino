06e3332c7fc932130836a0e44951d7059aaec366
mohitgoinka <54929491+mohitgoinka@users.noreply.github.com>
2019-10-21 20:20:15 +0530

    Add files via upload

---
+++ evaluation_phase_1/evaluation_washing_machine.ino
@@ -0,0 +1,34 @@
+int trig=5,echo=6;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(trig,OUTPUT);
+pinMode(echo,INPUT);
+pinMode(12,OUTPUT);//pin 12 is for red LED.
+pinMode(13,OUTPUT);//pin 13 is for green LED.
+Serial.begin(9600);
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+long durat,distance;
+digitalWrite(trig,LOW);
+delayMicroseconds(2);
+digitalWrite(trig,HIGH);                                    
+delayMicroseconds(10);
+digitalWrite(trig,LOW);
+durat=pulseIn(echo,HIGH);
+//we suppose that the height of the washing machine is 50 cm.
+//speed of light in air is approx. 350m/s, and in water is approx. 1500 m/s.
+//durat here represents time.
+//durat is in micro seconds.
+if(durat==1428.57)
+digitalWrite(12,HIGH);
+else if(durat==333.33){
+digitalWrite(13,HIGH);
+delay(20);
+digitalWrite(13,LOW);
+delay(20);
+}
+}
