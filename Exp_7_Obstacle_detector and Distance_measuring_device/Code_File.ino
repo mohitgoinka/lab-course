8f956ea834ef37408df278f87f3cc8bc24edda43
mohitgoinka <54929491+mohitgoinka@users.noreply.github.com>
2019-11-03 23:50:42 +0530

    Create Code_File.ino

---
+++ Exp_7_Obstacle_detector and Distance_measuring_device/Code_File.ino
@@ -0,0 +1,21 @@
+long d;
+void setup()
+{
+pinMode(10,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(11,INPUT);
+  Serial.begin(9600);
+}
+void loop()
+{
+digitalWrite(10,LOW);
+  delayMicroseconds(2);
+  digitalWrite(10,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(10,LOW);
+  d=pulseIn(11,HIGH);
+  long dist=(d*0.034)/2;
+  Serial.println(dist);
+ analogWrite(3,dist);
+ 
+}
